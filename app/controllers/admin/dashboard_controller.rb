290c5fb124e274a571a3c423105b834ae7984324
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2024-02-28 15:32:50 -0500

    ADD dasboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.count
+    @category_count = Category.count
+    @order_count = Order.count
   end
 end
