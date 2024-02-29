f7ebd091b52dbf279627cf0c1e9c6008046b1fc2
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2024-02-28 22:15:30 -0500

    ADD about us page and link it to footer

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+# app/controllers/about_controller.rb
+class AboutController < ApplicationController
+  def index
+    # You can add any static content about the store here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,11 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get 'about', to: 'about#index' 
+end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -71,4 +76,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
