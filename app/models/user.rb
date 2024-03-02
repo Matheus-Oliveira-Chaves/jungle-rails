6706fe71919d413fabcff2f8519ca8b3bbca6aa8
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2024-03-01 22:36:23 -0500

    ADD user model rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 5 }
 
-def admin?
-  self.email == ENV['ADMIN_EMAIL'] && self.authenticate(ENV['ADMIN_PASSWORD'])
-  
-end
+  def admin?
+    self.email == ENV['ADMIN_EMAIL'] && self.authenticate(ENV['ADMIN_PASSWORD'])
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.downcase.strip)
+    user && user.authenticate(password)
+  end
 end
